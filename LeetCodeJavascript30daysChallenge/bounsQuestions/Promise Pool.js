3247eabdc659ad181009e2f5faf6e49fa4c870a6
Encall27 <84779960+Encall27@users.noreply.github.com>
2023-06-11 18:38:17 +0800

    Update Promise Pool.js

--- LeetCodeJavascript30daysChallenge/bounsQuestions/Promise Pool.js
+++ LeetCodeJavascript30daysChallenge/bounsQuestions/Promise Pool.js
@@ -1,1 +1,38 @@
-
+/**
+ * @param {Function[]} functions
+ * @param {number} n
+ * @return {Function}
+ */
+var promisePool = async function(functions, n) {
+    
+    return new Promise((resolve,reject) => {
+      
+        let index = 0; 
+        let inProgress = 0;
+        
+        function runFunctions() {
+          
+            // all functions start run and no function inprogress => all done
+            if (index === functions.length && inProgress === 0) {
+                resolve();
+            }
+            
+            // 
+            while (index < functions.length && inProgress < n) {
+                console.log('now index: ', index, ' now inProgress :', inProgress)
+                const tS = new Date();
+                functions[index++]()
+                    .then(() => {
+                        const tE = new Date();
+                        console.log('functions', (index-1),  'duration', tE - tS)
+                        console.log('functions end time at :', tE - tStart)
+                        inProgress--;
+                        runFunctions();
+                   })
+                inProgress++;
+            }
+    
+        }
+        runFunctions();
+    });
+};
